26e05dda1ddb1f1b6a3ac30b19820e383dee1c52
Hugo Pobil <hpascual@mrhouston.net>
2024-03-02 13:52:00 +0100

    Merge remote-tracking branch 'origin/main'

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { StatusCodes } = require('http-status-codes');
 
 // Base de datos
 const DB_NAME = "productivity-ddbb";
-require('./config/db.config')
+require('./Config/db.config')
 
 // Creamos la instancia de la app
 const app = express();
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(logger('dev'));
 
 // Rutas
-const routes = require('./config/routes.config');
+const routes = require('./Config/routes.config');
 app.use('/', routes);
 
 /* Handle errors */
