602f81408fe6877efe9fcabe2092d8f0e2822b3f
Vladimir Ilovaca <vladoilovaca@gmail.com>
2024-03-10 21:18:51 +0100

    Updates

--- Config/nodemailer.config.js
+++ Config/nodemailer.config.js
@@ -10,11 +10,11 @@ module.exports.transporter = nodemailer.createTransport({
 
 module.exports.createEmailTemplate = (user) => {
   
-    return `<div style="margin: 24px;">
-    <h1>Validate your email 🚀</h1>
-    <p>Hi ${user.username} 👋🏻, thanks for registering!</p>
+    return `<div style="margin: 24px; border-color:#1400FF; border-width: medium; border-style: solid; padding-left: 25px; height: 190px; width: 350px">
+    <h1>Welcome to Achieve It 🎉</h1>
+    <p>Hi ${user.username} 👋🏻, thank you for join our comunity!</p>
     <p>Click on the following link to validate your email</p>
-    <a href="http://localhost:5173/activate/${user.activationToken}" style="background-color: green;color: whitesmoke;padding: 8px 12px;border-radius: 4px;text-decoration: none;">
+    <a href="http://localhost:5173/activate/${user.activationToken}" style="background-color: #1400FF;color: white;padding: 8px 12px;border-radius: 20px;text-decoration: none;">
         Activate
     </a>
 </div>`;
