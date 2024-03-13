42b99e1ec6ada30667d65c1094edc0a614348991
Hugo Pobil <hpascual@mrhouston.net>
2024-03-13 17:58:58 +0100

    update

--- Controllers/follower.controller.js
+++ Controllers/follower.controller.js
@@ -1,18 +1,18 @@
 const Follower = require("../models/Follower.model");
 
-// module.exports.followeUser = (req, res, next) => {
-//     Follower.findOne({ user: req.currentUserId, follower: req.params.followerId })
-//       .then((like) => {
-//         if (like) {
-//           Like.deleteOne({user: req.currentUserId, post: req.params.postId})
-//             .then(() => {
-//               res.json('like removed')
-//             })
-//         } else {
-//           Like.create({user: req.currentUserId, post: req.params.postId})
-//             .then(() => {
-//               res.json('like added')
-//             })
-//         }
-//       })
-//   };
+module.exports.followeUser = (req, res, next) => {
+    Follower.findOne({ user: req.currentUserId, follower: req.params.followerId })
+      .then((follow) => {
+        if (follow) {
+            Follower.deleteOne({user: req.currentUserId, follower: req.params.postId})
+            .then(() => {
+              res.json('like removed')
+            })
+        } else {
+          Follower.create({user: req.currentUserId, follower: req.params.postId})
+            .then(() => {
+              res.json('follower added')
+            })
+        }
+      })
+  };
